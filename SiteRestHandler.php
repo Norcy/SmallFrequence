ab82d159a6978ecec97ace24bc23f391d28a8315
Norcy <chenyg32@gmail.com>
2019-10-07 18:11:19 +0800

    Auto Commit [by ubuntu]

--- SiteRestHandler.php
+++ SiteRestHandler.php
@@ -1,17 +1,12 @@
 <?php 
+header('Content-Type:text/json;charset=utf-8');
 require_once("SimpleRest.php");
  
 class SiteRestHandler extends SimpleRest {
  
     function getAllSites() {    
         $json_string = file_get_contents('FM_List/regions.json');
-        $data = json_decode($json_string, true);
-        var_dump($data);
-    }
-    
-    public function encodeJson($responseData) {
-        $jsonResponse = json_encode($responseData);
-        return $jsonResponse;        
+    	echo $json_string;	
     }
     
     public function getSite($id) {
