08ad6f1815beb939ef958474602976d171806b00
NorcyChen <norcychen@tencent.com>
2019-11-06 11:55:05 +0800

    Fake [by Norcy]

--- SiteRestHandler.php
+++ SiteRestHandler.php
@@ -3,9 +3,9 @@ header('Content-Type:text/json;charset=utf-8');
 require_once("SimpleRest.php");
  
 class SiteRestHandler extends SimpleRest {
- 
+    $dic = "FM_List";
  	function getAllRegions($version) {    
-        $json_string = file_get_contents('FM_List/regions.json');
+        $json_string = file_get_contents($dic."/regions.json");
         // 用参数true把JSON字符串强制转成PHP数组  
         $data = json_decode($json_string, true);
         #echo $version;
@@ -17,7 +17,7 @@ class SiteRestHandler extends SimpleRest {
     }
 
     function getAllSites($version) {    
-        $json_string = file_get_contents('FM_List/all.json');
+        $json_string = file_get_contents($dic."/all.json");
     	$data = json_decode($json_string, true);
         if ($version == null || $version >= $data["version"]) {
 	    return;
@@ -26,7 +26,7 @@ class SiteRestHandler extends SimpleRest {
     }
     
     function getSite($id, $version) {
-        $json_string = file_get_contents('FM_List/'.$id.'.json');
+        $json_string = file_get_contents($dic.$id.'.json');
         $data = json_decode($json_string, true);
         if ($version == null || $version >= $data["version"]) {
             return;
